0b851e575ffdbee21ddcca3f96616ec6f5de2398
woxtu <woxtup@gmail.com>
2017-03-09 03:10:55 +0900

    Add userful Generators

--- Sources/FactorySwift/Generator.swift
+++ Sources/FactorySwift/Generator.swift
@@ -15,7 +15,34 @@ public struct Generator {
         self.apply = apply
     }
     
+    /// Create a Generator that returns generated value
     public static func generate(_ f: @escaping () -> Any) -> Generator {
         return Generator { _ in f() }
+    }
+    
+    /// Create a Generator that returns fixed value
+    public static func value(_ value: Any) -> Generator {
+        return Generator { _ in value }
+    }
+    
+    /// Create a Generator that returns sequential value
+    public static func sequence(_ f: @escaping (Int) -> Any) -> Generator {
+        return Generator { f($0.count) }
+    }
+    
+    /// Create a Generator that returns value depends another attribute
+    public static func depend(on name: String, _ f: @escaping (Any?) -> Any) -> Generator {
+        return Generator { f($0.rawValues[name]) }
+    }
+    
+    /// Create a Generator that returns value depends other attributes
+    public static func depend(on names: [String], _ f: @escaping ([Any?]) -> Any) -> Generator {
+        return Generator { context in f(names.map { context.rawValues[$0] }) }
+    }
+    
+    /// Create a Generator that returns value random picked from passed sequence
+    public static func pick<S: Sequence>(from sequence: S) -> Generator {
+        let array = Array(sequence)
+        return Generator { _ in array[Int(arc4random_uniform(UInt32(array.count)))] }
     }
 }
--- Tests/FactorySwiftTests/GeneratorTests.swift
+++ Tests/FactorySwiftTests/GeneratorTests.swift
@@ -15,4 +15,43 @@ class GeneratorTests: XCTestCase {
         let generator = Generator.generate { "Serval" }
         XCTAssertEqual(generator.apply(context) as? String, "Serval")
     }
+    
+    func testValue() {
+        let context = Context()
+        let generator = Generator.value("Serval")
+        XCTAssertEqual(generator.apply(context) as? String, "Serval")
+    }
+    
+    func testSequence() {
+        var context = Context()
+        let generator = Generator.sequence { $0 }
+        XCTAssertEqual(generator.apply(context) as? Int, 0)
+        
+        context.increment()
+        XCTAssertEqual(generator.apply(context) as? Int, 1)
+    }
+    
+    func testDepend() {
+        var context = Context()
+        context.rawValues["name"] = "Serval"
+        let generator = Generator.depend(on: "name") { "I'm \($0 as! String)!" }
+        XCTAssertEqual(generator.apply(context) as? String, "I'm Serval!")
+    }
+    
+    func testMultipleDepend() {
+        var context = Context()
+        context.rawValues["greeting"] = "Hi"
+        context.rawValues["name"] = "Serval"
+        let generator = Generator.depend(on: ["greeting", "name"]) {
+            let values = $0.flatMap { $0 as? String }
+            return "\(values[0]), I'm \(values[1])!"
+        }
+        XCTAssertEqual(generator.apply(context) as? String, "Hi, I'm Serval!")
+    }
+    
+    func testPick() {
+        let context = Context()
+        let generator = Generator.pick(from: 0 ..< 5)
+        XCTAssert((0 ..< 5).contains(generator.apply(context) as! Int))
+    }
 }
